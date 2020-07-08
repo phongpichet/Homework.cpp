986e85786716b4aee28d171202f126e5d4aa9ec4
phongpichet <68002849+phongpichet@users.noreply.github.com>
2020-07-08 16:03:13 +0700

    Add files via upload

---
+++ Homework.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void main()
+{	cout<<"	      #"<<endl;
+	cout<<"	   #     #"<<endl;
+	cout<<"	 #         #"<<endl;
+	cout<<"	#           #"<<endl;
+	cout<<"	# # # # # # #"<<endl;
+	cout<<"	#           #"<<endl;
+	cout<<"name:Phongpichet supcong"<<endl;
+}
